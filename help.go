ace41ce98ae2bab17b3abb897fe96083c020f616
William Rehwinkel <willrehwinkel@gmail.com>
2022-07-08 21:15:34 -0400

    Use os.Args[0] instead of hard-coded name in help text

--- help.go
+++ help.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"bytes"
 	_ "embed"
 	"fmt"
+	"os"
+	"text/template"
 )
 
 //go:embed help.txt
 var HelpText []byte
 
+type HelpTextStr struct {
+	Name string
+}
+
 func FullHelpHandler() {
-	fmt.Printf("\n%s\n", HelpText)
+	tmpl, err := template.New("help").Parse(string(HelpText))
+	if err != nil {
+		panic(err)
+	}
+
+	var out bytes.Buffer
+
+	if err = tmpl.Execute(&out, HelpTextStr{os.Args[0]}); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("\n%s\n", out.String())
 }
